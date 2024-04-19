0b8812b5acd662cd4fd731ee819a6d7ca4c237de
kaung min khant <kaungminkhant.official@gmail.como>
2024-04-19 11:00:15 +0630

    up to day 2 and one from day 3

---
+++ src/java21days/Investor.java
@@ -0,0 +1,11 @@
+package java21days;
+
+public class Investor {
+  public static void main(String[] args) {
+    float initial = 14_000;
+    initial = initial + initial * 0.4F;
+    initial = initial - 1_500;
+    initial = initial + initial * 0.12F;
+    System.out.println("The final amount after three year is " + initial);
+  } 
+}
---
+++ src/java21days/Variables.java
@@ -0,0 +1,13 @@
+package java21days;
+public class Variables {
+  public static void main(String[] args) {
+    final char UP = 'U';
+    byte initialLevel = 12;
+    short location = 12345;
+    int score = 23466;
+    boolean newGame = true;
+
+    System.out.println("Level: " + initialLevel);
+    System.out.println("Up: " + UP);
+  } 
+}
